b39996aa565acb337e2261ed7ceca24d7e7bac5e
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2025-11-14 09:41:57 -0600

    hash, compresion RLE, firma digital

--- main.py
+++ main.py
@@ -1,6 +1,64 @@
 import os
+import heapq
+import rsa
+
+def fnv1_hash(text):
+    FNV_prime = 16777619
+    offset_basis = 2166136261
+
+    hash_value = offset_basis
+    for char in text.encode("utf-8"):
+        hash_value *= FNV_prime
+        hash_value ^= char
+        hash_value &= 0xffffffff  # Mantener 32 bits
+
+    return hash_value
+
+def comprimir_rle(text):
+    if not text:
+        return ""
+
+    resultado = []
+    conteo = 1
+
+    for i in range(1, len(text)):
+        if text[i] == text[i - 1]:
+            conteo += 1
+        else:
+            resultado.append(text[i - 1] + str(conteo))
+            conteo = 1
+
+    resultado.append(text[-1] + str(conteo))
+    return "".join(resultado)
+
+def generar_claves():
+        public_key, private_key = rsa.newkeys(512)
+
+        with open("clave_publica.pem", "wb") as f:
+            f.write(public_key.save_pkcs1("PEM"))
+        with open("clave_privada.pem", "wb") as f:
+            f.write(private_key.save_pkcs1("PEM"))
+        return public_key, private_key
+
+def firmar_mensaje(mensaje):
+
+    with open("archivo_mensaje.bin","wb") as f:
+        f.write(mensaje)
+    with open("clave_privada.pem", "rb") as f:
+        private_key = rsa.PrivateKey.load_pkcs1(f.read())
+
+    with open("archivo_mensaje", "rb") as f:
+        mensaje_ = f.read()
+
+    firma = rsa.sign(mensaje_, private_key, "SHA-256")
+
+    with open("firma.bin", "wb") as f:
+                f.write(firma)
+
+
 def menu():
     mensaje = ""
+    hash =""
     while True:
         limpiar()
         print("ENVÍO Y VERIFICACIÓN DE MENSAJES\n")
@@ -24,14 +82,29 @@ def menu():
         elif opcion == "2":
             limpiar()
             print(f"Mensaje actual: {mensaje}\n")
+            print("\nHASH FNV-1")
+            print(f"\nHash generado: {fnv1_hash(mensaje)}")
+            hash = fnv1_hash(mensaje)
             input("\nPresione 'Enter' para continuar.")
         elif opcion == "3":
             limpiar()
             print(f"Mensaje actual: {mensaje}\n")
+            print("\nCOMPRESIÓN RLE\n")
+            rle_comprimido = comprimir_rle(mensaje)
+
+            print("Tamaño original:", len(mensaje), "caracteres")
+            print("Tamaño comprimido:", len(rle_comprimido), "caracteres")
             input("\nPresione 'Enter' para continuar.")
         elif opcion == "4":
             limpiar()
             print(f"Mensaje actual: {mensaje}\n")
+            print(f"Hash actual: {hash}")
+            print("\nFIRMAR HASH CON CLAVE PRIVADA\n")
+            clave_publica, clave_privada = generar_claves()
+            print(f"Clave publica: {clave_publica}")
+            print(f"Clave privada{clave_privada}")
+            firmar_mensaje(mensaje)
+
             input("\nPresione 'Enter' para continuar.")
         elif opcion == "5":
             limpiar()
